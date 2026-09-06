72017ea7dfe9d0b5859725de33224b2165f13977
janaa7 <J.Abd2357@nu.edu.eg>
2026-09-06 14:51:46 +0300

    Add hooks functionality to movie wishlist

--- Movie-WishList/src/App.tsx
+++ Movie-WishList/src/App.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import MovieList from "./components/MovieList";
-import Movie from "./data/movie";
+import moviesData from "./data/movie";
 
 function App() {
+  const [movies, setMovies] = useState(moviesData);
+
+  function markWatched(id: number) {
+    setMovies(
+      movies.map(movie =>
+        movie.id === id
+          ? { ...movie, watched: true }
+          : movie
+      )
+    );
+  }
+
   return (
     <div className="app-container">
       <header className="header">
         <h1>My Shows</h1>
       </header>
+
       <main>
-        <MovieList movies={Movie} />
+        <MovieList
+          movies={movies}
+          markWatched={markWatched}
+          
+        />
       </main>
     </div>
   );
--- Movie-WishList/src/components/MovieCard.tsx
+++ Movie-WishList/src/components/MovieCard.tsx
@@ -2,17 +2,20 @@ import type { Movie } from "../data/movie";
 
 interface MovieCardProps {
   movie: Movie;
+  markWatched: (id: number) => void;
 }
 
-function MovieCard({ movie }: MovieCardProps) {
+function MovieCard({ movie, markWatched }: MovieCardProps) {
   return (
     <div className="movie-card">
+
       <div className="poster-wrapper">
         <img
           src={movie.poster}
           alt={movie.title}
           className="movie-poster"
         />
+
         <span className={`badge ${movie.watched ? "watched" : "unwatched"}`}>
           {movie.watched ? "Watched" : "Not Watched"}
         </span>
@@ -20,11 +23,20 @@ function MovieCard({ movie }: MovieCardProps) {
 
       <div className="movie-content">
         <h3 className="movie-title">{movie.title}</h3>
+
         <div className="movie-meta">
           <span className="movie-genre">{movie.genre}</span>
           <span className="movie-year">{movie.year}</span>
         </div>
+
+        <button
+  className="watch-button"
+  onClick={() => markWatched(movie.id)}
+>
+  Mark Watched
+</button>
       </div>
+
     </div>
   );
 }
